288a5778f4a538c0f48db8906d6c7ea7fb0ddb58
yaswanthvitta <yaswanthvitta@gmail.com>
2024-06-27 10:08:42 +0530

    This is new-fearure5

--- App.js
+++ App.js
@@ -2,4 +2,6 @@ const App = () =>{
     console.log("This is  new-feature1")
     console.log("This is new-feature2")
     console.log("This is new-feature3")
+    // added feature 5
+    console.log("This is new-feature5")
 }
